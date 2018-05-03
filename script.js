d0b3a2895be8114373c26bba37d66aee728394fa
Kevin Marshall <marshall_k@live.com>
2018-05-02 21:25:03 -0700

    Create grid generation function.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let container = document.getElementById("container");
+function generateGrid (gridSize = 16) {
+	for ( i = 0; i < gridSize; ++i) {
+		let newDiv = document.createElement("div");
+		container.appendChild(newDiv);
+	}
+
+}
