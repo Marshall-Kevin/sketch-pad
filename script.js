71210430b7dfffd4b17ff03688bea08d82e9e14b
Kevin Marshall <marshall_k@live.com>
2018-05-04 20:24:04 -0700

    Complete grid auto generation.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 const container = document.getElementById("container");
 function generateGrid (gridSize = 16) {
-	for ( i = 0; i < gridSize; ++i) {
+	for ( i = 0; i < Math.pow(gridSize, 2); ++i) {
 		let newDiv = document.createElement("div");
 		container.appendChild(newDiv);
+		container.style.setProperty("--gridSize", gridSize);
 	}
 
 }
