a744b36c549c10e2ff427d3f75e36bfb54fb11f5
Kevin Marshall <marshall_k@live.com>
2018-05-07 19:29:46 -0700

    Event listeners working for hover even.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const container = document.getElementById("container");
+generateGrid();
 function generateGrid (gridSize = 16) {
 	for ( i = 0; i < Math.pow(gridSize, 2); ++i) {
 		let newDiv = document.createElement("div");
@@ -7,4 +8,18 @@ function generateGrid (gridSize = 16) {
 		container.style.setProperty("--gridSize", gridSize);
 	}
 
+}
+let childNodes = container.querySelectorAll("div");
+for (i = 0; i < childNodes.length; ++i) {
+	childNodes[i].addEventListener("mouseenter", function(e) {
+		this.classList.add("hovered")
+	});
+}		
+
+function darkenDiv () {
+	//let increment = 0;
+	//increment += 10;
+	this.classList.add("hovered");
+	//this.style.setProperty("--increment", increment);
+	return;
 }
